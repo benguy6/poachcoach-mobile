63267355d915741684634120fb72cd2a41e8779b
ushreyas <shreyasuppalapati@gmail.com>
2025-05-13 23:06:39 +0800

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ezdbjkcrtepdihmyunzm.supabase.co';
-const supabaseAnonKey = process.env.SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
